072b929c2e7bd0a30c99f5b45260442e06b2e74f
Ben Doshna <benjamin@doshna.net>
2025-11-30 19:30:04 -0500

    More Linear Analysis Plots

--- firstcount.jl
+++ firstcount.jl
@@ -43,7 +43,7 @@ put_stim_in!(df,allmoths,"2024_11_11")
 ##
 d = combine(groupby(df,[:moth,:wb,:muscle])) do gdf 
     (
-    trial = gdf.trial[1] =="pre" ? 0 : 1,
+    trial = gdf.trial[1] == "pre" ? 0 : 1, 
     species=gdf.species[1],
     firsttime= minimum(gdf.time),
     firstphase = minimum(gdf.phase),
@@ -60,7 +60,7 @@ countmat = Matrix(dropmissing((select(unstack(count,:muscle,:count),Not(:wb)))))
 time = select(d,:wb,:muscle,:firstphase,:stim)
 timemat = Matrix(dropmissing(select(unstack(time,:muscle,:firstphase),Not(:wb))))
 tris = Bool.(countmat[:,1])
-stim = countmat[:,2]
+stim = (countmat[:,2] .- mean(countmat[:,2])) .* pixel_conversion
 
 fullmat = zscore(timemat[:,2:end])
 
@@ -70,18 +70,34 @@ pc = fit(MultivariateStats.PCA,fullmat')
 lat = pc.proj' * fullmat'
 
  
-##
+## Pc projection 
 F = Figure() 
-ax = Axis3(F[1,1])
-scatter!(ax,lat[1,:],lat[2,:],lat[3,:],color=stim)
+ax = Axis(F[1,1],title="2024_11_08", xlabel = "pc1",ylabel = "pc2")
+scatter!(ax,lat[1,:],lat[2,:],color=[x ? :firebrick : :steelblue for x in tris])
+F
+## Proj vs stim position 
+F = Figure() 
+ax = Axis(F[1,1],title="pc_1",xlabel="FLower Position (mm)")
+scatter!(ax,stim,lat[1,:],color=[x ? :firebrick : :steelblue for x in tris])
+ax1 = Axis(F[2,1],title="pc_2",xlabel="FLower Position (mm)")
+scatter!(ax1,stim,lat[2,:],color=[x ? :firebrick : :steelblue for x in tris])
+ax1 = Axis(F[3,1],title="pc_3",xlabel="FLower Position (mm)")
+scatter!(ax1,stim,lat[3,:],color=[x ? :firebrick : :steelblue for x in tris])
+
 F
 ##
 dlm = d[d.muscle.=="ldlm" .|| d.muscle.=="rdlm",[:wb,:muscle,:firsttime,:trial,:stim]]
 timemat = dropmissing(select(unstack(dlm,:muscle,:firsttime),Not(:wb)))
-timemat.off = timemat.rdlm - timemat.ldlm 
+timemat.off = 1000 .*(timemat.rdlm - timemat.ldlm)
 
 ##
+sdlm = timemat[timemat.trial.=="pre",:]
+fdlm = timemat[timemat.trial.=="post",:]
+
+
+## 
 F = Figure() 
-ax = Axis(F[1,1])
-scatter!(ax,timemat.off,color=timemat.stim)
+ax = Axis(F[1,1],xlabel= "Wing Beat Number",ylabel="L-R DLM Timing Offset (ms)",title = "2024_11_08")
+scatter!(ax,sdlm.off,color=:steelblue)
+scatter!(ax,fdlm.off,color=:firebrick)
 F
