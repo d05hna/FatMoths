24999870d800897782e9333cc48eb42f0e1bb53a
Ben Doshna <benjamin@doshna.net>
2026-01-21 12:22:31 -0500

    Updating OL stats with new tf_stats function

--- FreeFlightStats_OL.jl
+++ FreeFlightStats_OL.jl
@@ -1,31 +1,13 @@
-using CSV
-using DataFrames
-using Pipe
-using Glob
-using CairoMakie
-using HDF5
-using Interpolations
+using CSV 
+using DataFrames 
+using MAT 
+using DataFrames 
+using CairoMakie 
+using Statistics 
 using LinearAlgebra
-using StatsBase
-using Optim
-using DSP
-using DelimitedFiles
-using DataFramesMeta
-using FFTW
-using AlgebraOfGraphics
 using JLD2
-using Colors
-using GLM
-using SavitzkyGolay
-using MultivariateStats
-using Distributions
-using QuadGK
-using ProgressMeter
-using MAT
-# using CairoMakie
-using HypothesisTests
-include("/home/doshna/Documents/PHD/comparativeMPanalysis_bmd/functions.jl")
-include("me_functions.jl")
+using DSP 
+include("stats_functions.jl")
 CairoMakie.activate!()
 theme = theme_minimal()
 # theme.palette = (; color = ColorSchemes.tab20)
@@ -38,60 +20,66 @@ pixel_conversion = 0.14 ## mm/pixels
 
 
 freqqs = [0.2000,0.3000,0.5000,0.7000,1.100,1.300,1.700,1.900,2.300,2.900,3.700,4.300,5.300,6.100,7.900,8.900,11.30,13.70]
-##
 
 pnt = matread("Steven/steven_data_linear.mat")["results"]
 Ci = hcat(pnt["Ci"]...)
 Cf = hcat(pnt["Cf"]...)
-
-
-
-
+##
 pre = DataFrame() 
 post = DataFrame() 
 
-@showprogress for i in 1:18 
-    realimagi = hcat(real.(Ci[i,:]),imag.(Ci[i,:]))
-    coi = cov(realimagi)
-    meandi = mean(realimagi,dims=1)[:]
-
-    realimagf = hcat(real.(Cf[i,:]),imag.(Cf[i,:]))
-    cof = cov(realimagf)
-    meandf = mean(realimagf,dims=1)[:]
-
-
-    gi,ti = confMagPhase(meandi,coi,0.95)
-    mg = norm(meandi)
-    mp = atan(meandi[2],meandi[1])
-    tmp = Dict("freq"=> freqqs[i],"mg" => mg,"mp"=>mp,"glow"=>mg - gi[1],"ghigh" => gi[2]-mg,"plow"=>mp - ti[1],"phigh"=>ti[2]-mp)
-    push!(pre,tmp,cols=:union)
-
-    gf,tf = confMagPhase(meandf,cof,0.95)
-    mg = norm(meandf)
-    mp = atan(meandf[2],meandf[1])
-    tmp = Dict("freq"=> freqqs[i],"mg" => mg,"mp"=>mp,"glow"=>mg- gf[1],"ghigh" => gf[2]-mg,"plow"=>mp-tf[1],"phigh"=>tf[2]-mp)
-    push!(post,tmp,cols=:union)
+for (i,f) in enumerate(freqqs)
+    mg,glow,ghigh,mp,p_std = tf_stats(Ci[i,:])
+    tmp = Dict(
+        "freq"  => f,
+        "mg"    => mg,
+        "glow"  => glow,
+        "ghigh" => ghigh,
+        "mp"    => mp,
+        "stp"  => p_std,
+    )
+    push!(pre, tmp,cols=:union)
+    mg,glow,ghigh,mp,p_std = tf_stats(Cf[i,:])
+    tmp = Dict(
+        "freq"  => f,
+        "mg"    => mg,
+        "glow"  => glow,
+        "ghigh" => ghigh,
+        "mp"    => mp,
+        "stp"  => p_std,
+    )
+    push!(post, tmp,cols=:union)
 end
-pre.mp = unwrap_negative(pre.mp)
-post.mp = unwrap_negative(post.mp)
+pre.mp = unwrap(pre.mp)
+post.mp = unwrap(post.mp)
+CSV.write("FreeFlight_OL_stats_pre.csv",pre)
+CSV.write("FreeFlight_OL_stats_post.csv",post)
 
-##
+## Per the Sig Tracking Results, Only keeping frequencies below 8.9 Hz
 pre = pre[pre.freq .< 8.9,:]
 post = post[post.freq .< 8.9,:]
 ##
 F = Figure(size=(800,800)) 
-ax = Axis(F[2,1],ylabel="Gain",xscale=log10,xticklabelsvisible=false,yticks=[0,0.1,1],yscale=log10)
+ax = Axis(F[2,1],xscale = log10,xticklabelsvisible=false, 
+    ylabel="Gain", yticks = ([0.01,0.1,1],[L"0.01",L"0.1",L"1"]),yscale=log10,
+    ylabelsize=30, yticklabelsize=25,
+    )
 
 lines!(ax,pre.freq,pre.mg,color=:steelblue,linewidth=3)
 lines!(ax,post.freq,post.mg,color=:firebrick,linewidth=3)
-errorbars!(ax,pre.freq,pre.mg,pre.glow,pre.ghigh,color=:steelblue,whiskerwidth=10)
-errorbars!(ax,post.freq,post.mg,post.glow,post.ghigh,color=:firebrick,whiskerwidth=10)
-ax2 = Axis(F[3,1],ylabel="Phase (radians)",xlabel="Frequency (Hz)",xscale=log10,xticks=[0.1,1,10],limits=(0.1,nothing,nothing,nothing))
+band!(ax,pre.freq,pre.glow,pre.ghigh,color=:steelblue,alpha=0.3)
+band!(ax,post.freq,post.glow,post.ghigh,color=:firebrick,alpha=0.3)
+ax2 = Axis(F[3,1],limits=(0.1,nothing,nothing,nothing),
+    ylabel="Phase (radians)", yticks = ([0,-pi/2, -pi,-3pi/2], [L"0",L"-\frac{\pi}{2}",L"-\pi",L"-\frac{3\pi}{2}"]),
+    ylabelsize=30, yticklabelsize=25,
+    xlabel="Frequency (Hz)",xscale=log10,xticks=([0.1,1,8],[L"0.1",L"1",L"8"]),
+    xlabelsize=30, xticklabelsize=25,
+    )
 
 l = lines!(ax2,pre.freq,pre.mp,color=:steelblue,linewidth=3)
 h = lines!(ax2,post.freq,post.mp,color=:firebrick,linewidth=3)
-errorbars!(ax2,pre.freq,pre.mp,pre.plow,pre.phigh,whiskerwidth=10,color=:steelblue)
-errorbars!(ax2,post.freq,post.mp,post.plow,post.phigh,whiskerwidth=10,color=:firebrick)
+band!(ax2,pre.freq,pre.mp .- pre.stp,pre.mp .+ pre.stp,color=:steelblue,alpha=0.3)
+band!(ax2,post.freq,post.mp .- post.stp,post.mp .+ post.stp,color=:firebrick,alpha=0.3)
 
 linkxaxes!(ax2,ax)
 Legend(F[1,1],[l,h],["Low Mass","High Mass"],orientation = :horizontal)
@@ -102,6 +90,7 @@ gci = abs.(Ci)[1:15,:]
 gcf = abs.(Cf)[1:15,:]
 
 gchange = gcf ./ gci
+
 mgchange = mean(gchange,dims=2)[:]
 semchange = std(gchange,dims=2)[:] ./sqrt(10)
 
@@ -118,7 +107,7 @@ ax.xlabel="Frequency (Hz)"
 ax.ylabel = "Gain Change Multiple"
 barplot!(ax,freqqs[1:15],mgchange,width=widths,color=:grey)
 errorbars!(ax,freqqs[1:15],mgchange,semchange,color=:black)
-lines!(ax, range(0.15,10,length=16),repeat([1.0],16),linestyle=:dash,color=:red,label="No Change")
+lines!(ax, range(0.15,10,length=16),repeat([1.7],16),linestyle=:dash,color=:red,label="No Change")
 axislegend(ax)
-save("Figs/PaperFigs/barplot_OL.svg",f,px_per_unit=4)
+# save("Figs/PaperFigs/barplot_OL.svg",f,px_per_unit=4)
 f
