ce6e0f307751705059ad361eb604a82beab2629e
Ben Doshna <benjamin@doshna.net>
2026-01-21 11:46:13 -0500

    updated CL Free Flight Figs with fixed stats

--- FreeFlightStats_CL.jl
+++ FreeFlightStats_CL.jl
@@ -1,30 +1,12 @@
-using CSV
-using DataFrames
-using Pipe
-using Glob
 using CairoMakie
-using HDF5
-using Interpolations
+using FFTW 
+using CSV 
+using JLD2 
+using Statistics 
+using DataFrames
 using LinearAlgebra
-using StatsBase
-using Optim
-using DSP
-using DelimitedFiles
-using DataFramesMeta
-using FFTW
-using AlgebraOfGraphics
-using JLD2
-using Colors
-using GLM
-using SavitzkyGolay
-using MultivariateStats
-using Distributions
-using QuadGK
-using ProgressMeter
-# using CairoMakie
-using HypothesisTests
-include("/home/doshna/Documents/PHD/comparativeMPanalysis_bmd/functions.jl")
-include("me_functions.jl")
+using DSP 
+include("stats_functions.jl")
 CairoMakie.activate!()
 theme = theme_minimal()
 # theme.palette = (; color = ColorSchemes.tab20)
@@ -33,15 +15,15 @@ theme.fontsize = 20
 set_theme!(theme)
 
 @load "fat_moth_free_flight.jld" FreeFlight
-pixel_conversion = 0.14 ## mm/pixels 
 
 
 freqqs = [0.2000,0.3000,0.5000,0.7000,1.100,1.300,1.700,1.900,2.300,2.900,3.700,4.300,5.300,6.100,7.900,8.900,11.30,13.70]
+N = 2000 
+fs = 100
 ##
 Hi = zeros(18,10) |> x -> Complex.(x)
 Hf = zeros(18,10) |> x -> Complex.(x)
-N = 2000
-fr = round.(fftfreq(N,100),digits=4)
+fr = round.(fftfreq(N,fs),digits=4)
 stimidx= [i for i in 1:N if fr[i] in freqqs]
 
 
@@ -60,49 +42,52 @@ end
 ##
 pre = DataFrame() 
 post = DataFrame() 
-
-@showprogress for i in 1:18 
-    realimagi = hcat(real.(Hi[i,:]),imag.(Hi[i,:]))
-    coi = cov(realimagi)
-    meandi = mean(realimagi,dims=1)[:]
-
-    realimagf = hcat(real.(Hf[i,:]),imag.(Hf[i,:]))
-    cof = cov(realimagf)
-    meandf = mean(realimagf,dims=1)[:]
-
-
-    gi,ti = confMagPhase(meandi,coi,0.95)
-    mg = norm(meandi)
-    mp = atan(meandi[2],meandi[1])
-    tmp = Dict("freq"=> freqqs[i],"mg" => mg,"mp"=>mp,"glow"=>mg - gi[1],"ghigh" => gi[2]-mg,"plow"=>mp - ti[1],"phigh"=>ti[2]-mp)
+for (i,f) in enumerate(freqqs)
+    mg,glow,ghigh,mp,std_p = tf_stats(Hi[i,:])
+    tmp = Dict(
+        "freq" => f, 
+        "mg" => mg,
+        "glow" => glow,
+        "ghigh" => ghigh,
+        "mp" => mp,
+        "stp" => std_p
+    )
     push!(pre,tmp,cols=:union)
-
-    gf,tf = confMagPhase(meandf,cof,0.95)
-    mg = norm(meandf)
-    mp = atan(meandf[2],meandf[1])
-    tmp = Dict("freq"=> freqqs[i],"mg" => mg,"mp"=>mp,"glow"=>mg- gf[1],"ghigh" => gf[2]-mg,"plow"=>mp-tf[1],"phigh"=>tf[2]-mp)
+    mg,glow,ghigh,mp,std_p = tf_stats(Hf[i,:])
+    tmp = Dict(
+        "freq" => f, 
+        "mg" => mg,
+        "glow" => glow,
+        "ghigh" => ghigh,
+        "mp" => mp,
+        "stp" => std_p
+    )
     push!(post,tmp,cols=:union)
 end
-pre.mp = unwrap_negative(pre.mp)
-post.mp = unwrap_negative(post.mp)
-##
+pre.mp = unwrap(pre.mp)
+post.mp = unwrap(post.mp)
 CSV.write("Steven/CL_pre_stats.csv",pre)
 CSV.write("Steven/CL_post_stats.csv",post)
 ##
 F = Figure(size=(800,800)) 
-ax = Axis(F[2,1],ylabel="Gain",xscale=log10,xticklabelsvisible=false,yticks=[0,0.1,1],yscale=log10)
+ax = Axis(F[2,1],ylabel="Gain",xscale=log10,
+    xticklabelsvisible=false,yticks=([0,0.1,1],[L"0",L"0.1",L"1"]),yscale=log10,yticklabelsize=25,
+    ylabelsize=30)
 
 lines!(ax,pre.freq,pre.mg,color=:steelblue,linewidth=3)
 lines!(ax,post.freq,post.mg,color=:firebrick,linewidth=3)
-errorbars!(ax,pre.freq,pre.mg,pre.glow,pre.ghigh,color=:steelblue,whiskerwidth=10)
-errorbars!(ax,post.freq,post.mg,post.glow,post.ghigh,color=:firebrick,whiskerwidth=10)
-ax2 = Axis(F[3,1],ylabel="Phase (radians)",xlabel="Frequency (Hz)",xscale=log10,xticks=[0.1,1,10],limits=(0.1,nothing,nothing,nothing))
+band!(ax,pre.freq,pre.glow,pre.ghigh,color=:steelblue,alpha=0.3)
+band!(ax,post.freq,post.glow,post.ghigh,color=:firebrick,alpha=0.3)
+
+ax2 = Axis(F[3,1],ylabel="Phase (radians)",xlabel="Frequency (Hz)",
+    xscale=log10,xticks=([0.1,1,10],[L"0.1",L"1",L"10"]),limits=(0.1,nothing,nothing,nothing),
+    yticks = ([0,-pi,-2pi, -3pi], [L"0",L"-\pi",L"-2\pi",L"-3\pi"]),yticklabelsize=25,xticklabelsize=25,
+    ylabelsize=30,xlabelsize=30)
 
 l = lines!(ax2,pre.freq,pre.mp,color=:steelblue,linewidth=3)
 h = lines!(ax2,post.freq,post.mp,color=:firebrick,linewidth=3)
-errorbars!(ax2,pre.freq,pre.mp,pre.plow,pre.phigh,whiskerwidth=10,color=:steelblue)
-errorbars!(ax2,post.freq,post.mp,post.plow,post.phigh,whiskerwidth=10,color=:firebrick)
-
+band!(ax2,pre.freq,pre.mp .- pre.stp,pre.mp .+ pre.stp,color=:steelblue,alpha=0.3)
+band!(ax2,post.freq,post.mp .- post.stp,post.mp .+ post.stp,color=:firebrick,alpha=0.3)
 linkxaxes!(ax2,ax)
 Legend(F[1,1],[l,h],["Low Mass","High Mass"],orientation = :horizontal)
 save("Figs/PaperFigs/CL_tracking.svg",F,px_per_unit=4)
@@ -126,18 +111,26 @@ dipost = (ipost .-0) .^2
 epre = sqrt.(drpre .+ dipre)
 epost = sqrt.(drpost .+ dipost)
 
-meanepre = mean(epre,dims=2)[:]
+meanpre = mean(epre,dims=2)[:]
 meanpost = mean(epost,dims=2)[:]
 
-ci_pre = [ci_low_n(epre[i,:]) for i in 1:18]
-ci_post = [ci_low_n(epost[i,:]) for i in 1:18]
+stdpre = std(epre,dims=2)[:]
+stdpost = std(epost,dims=2)[:]  
+
+lowpre = meanpre .- stdpre ./ sqrt(size(Hi,2))
+highpre = meanpre .+ stdpre ./ sqrt(size(Hi,2))
+lowpost = meanpost .- stdpost ./ sqrt(size(Hf,2))
+highpost = meanpost .+ stdpost ./ sqrt(size(Hf,2))
+
 
 F = Figure(size=(800,400))
-ax = Axis(F[1,1],xlabel="Frequency",xscale=log10,xticks=[0.1,1,10],
-    ylabel = "Tracking Error",yticks=[0,1,2],limits=(0.1,nothing,nothing,nothing))
-lines!(ax,freqqs,meanepre,linewidth=3,color=:steelblue)
-errorbars!(ax,freqqs,meanepre,ci_pre,whiskerwidth=10,color=:steelblue)
+ax = Axis(F[1,1],xscale=log10,
+    xlabel="Frequency (Hz)",xticks=([0.1,1,10],[L"0.1",L"1",L"10"]),xlabelsize=30,xticklabelsize=25,
+    ylabel = "Tracking Error",yticks=([0,1,2],[L"0",L"1",L"2"]), yticklabelsize=25,ylabelsize=30,
+    limits=(0.1,nothing,nothing,nothing))
+lines!(ax,freqqs,meanpre,linewidth=3,color=:steelblue)
+band!(ax,freqqs,lowpre,highpre,color=:steelblue,alpha=0.3)
 lines!(ax,freqqs,meanpost,linewidth=3,color=:firebrick)
-errorbars!(ax,freqqs,meanpost,ci_post,whiskerwidth=10,color=:firebrick)
+band!(ax,freqqs,lowpost,highpost,color=:firebrick,alpha=0.3)
 save("Figs/PaperFigs/Tracking_error.svg",F,px_per_unit=4)
 F
